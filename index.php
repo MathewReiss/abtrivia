c5ec7acfe4ecbe0f2dd1e8c4e21ca1b96bac51ba
MathewReiss <mathewreiss@gmail.com>
2018-08-03 16:50:11 -0400

    Spit polish

--- index.php
+++ index.php
@@ -5,18 +5,13 @@
 	header("Access-Control-Allow-Methods: GET");
 	header("Content-Type: application/json");
 
-	function pg_connection_string_from_database_url() {
-		extract(parse_url($_ENV["DATABASE_URL"]));
-		return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . " sslmode=require";
-	}
-
 	$conn = pg_connect(getenv("DATABASE_URL"));
 
 	$myquery = "SELECT data FROM questions ORDER BY day DESC LIMIT 1";
 	$result = pg_query($conn, $myquery);
 
 	$row = pg_fetch_row($result);
-	echo "Results: $row[0]";
+	echo "$row[0]";
 	
 	pg_close();
 
