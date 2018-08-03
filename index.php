403d0ae36b153a50df20d0414d0570d409348564
MathewReiss <mathewreiss@gmail.com>
2018-08-03 16:48:05 -0400

    Cleanup

--- index.php
+++ index.php
@@ -10,26 +10,14 @@
 		return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . " sslmode=require";
 	}
 
-	$conn = pg_connect(pg_connection_string_from_database_url()) or die(pg_last_error());
+	$conn = pg_connect(getenv("DATABASE_URL"));
 
-	//if(pg_connection_status() === PGSQL_CONNECTION_OK){
-		$myquery = "SELECT data FROM questions ORDER BY day DESC LIMIT 1";
-		$result = pg_query($conn, $myquery);
+	$myquery = "SELECT data FROM questions ORDER BY day DESC LIMIT 1";
+	$result = pg_query($conn, $myquery);
 
-		if(!$result) {
-			//Query Error
-			echo "Query Error!";
-			exit;
-		}
-
-		$row = pg_fetch_row($result);
-		echo "Results: $row[0]";
-	//} else {
-		//Connection Error
-		//echo "Connection Error!";
-		//exit;
-	//}
-
+	$row = pg_fetch_row($result);
+	echo "Results: $row[0]";
+	
 	pg_close();
 
 ?>
