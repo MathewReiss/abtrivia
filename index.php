1604ce3ffd30d7fc21a38a3013d587237e0cbedf
MathewReiss <mathewreiss@gmail.com>
2018-08-07 15:29:47 -0400

    API Folder

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-
-	header("Access-Control-Allow-Headers: Content-Type");
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Methods: GET");
-	header("Content-Type: application/json");
-
-	$conn = pg_connect(getenv("DATABASE_URL"));
-
-	$myquery = "SELECT * FROM questions ORDER BY day DESC LIMIT 1";
-	$result = pg_query($conn, $myquery);
-
-	$row = pg_fetch_row($result);
-	echo '{ "date" : "' . $row[0] . '", "questions" : ' . $row[1] . ' }';
-	
-	pg_close();
-
+	echo "Coming soon!"
 ?>
