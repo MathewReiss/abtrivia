304e404e48efcf7a910046b3ec0afb7d790217d0
MathewReiss <mathewreiss@gmail.com>
2018-08-17 15:45:07 -0400

    Logging

--- api/update.php
+++ api/update.php
@@ -15,6 +15,8 @@
 
 	$row = pg_fetch_row($result);
 
+	echo "<script>console.log('Row Returned: " . $row . "');</script>";
+
 	$today = date('Y-m-d');
 
 	$streak = $row ? $row[1] : 0;
@@ -28,7 +30,10 @@
 	$currentcontinuous = $row ? $row[9] : 0;
 	$lastgameperfect = $row ? $row[10] : false;
 
+	echo "<script>console.log('Row: " . $streak . ", " . $accuracy . ", " . $continuous . "');</script>";
+
 	if($today == $lastgame + (date(w) == 1 ? 3 : 1)) {
+		echo "<script>console.log('Played continuously');</script>";
 		$currentcontinuous++;
 		if($lastgameperfect) {
 			$currentstreak += $correct;
@@ -63,6 +68,11 @@
 					numcorrect = EXCLUDED.numcorrect, 
 					currentcontinuous = EXCLUDED.currentcontinuous, 
 					lastgameperfect = EXCLUDED.lastgameperfect;";
+
+	echo $myquery;
+
 	$result = pg_query($conn, $myquery);
+
+	pg_close();
 
 ?>
