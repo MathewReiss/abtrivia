1d49281dda771e588185e689a493c07ca113c946
MathewReiss <mathewreiss@gmail.com>
2018-08-18 10:01:01 -0400

    bool to int

--- api/update.php
+++ api/update.php
@@ -27,12 +27,12 @@
 	$numgamesplayed = $row ? $row[7] : 0;
 	$numcorrect = $row ? $row[8] : 0;
 	$currentcontinuous = $row ? $row[9] : 0;
-	$lastgameperfect = $row ? $row[10] : false;
+	$lastgameperfect = $row ? $row[10] : 0;
 
 	//if($today == $lastgame + (date(w) == 1 ? 3 : 1)) {
 	if(true) {
 		$currentcontinuous++;
-		if($lastgameperfect == true) {
+		if($lastgameperfect == 1) {
 			echo "Last game was perfect $lastgameperfect";
 			$currentstreak += $correct;
 		} else {
@@ -47,7 +47,7 @@
 	$continuous = max($continuous, $currentcontinuous);
 	$streak = max($streak, $currentstreak);
 
-	$lastgameperfect = ($correct == 6 ? true : false);
+	$lastgameperfect = ($correct == 6 ? 1 : 0);
 
 	$numcorrect += $correct;
 	$numgamesplayed++;
@@ -58,7 +58,7 @@
 
 	$myquery = "
 				INSERT INTO users
-				VALUES('" . $fitbit . "', " . $streak . ", " . $accuracy . ", " . $continuous . ", '" . $display . "', '" . $lastgame . "', " . $currentstreak . ", " . $numgamesplayed . ", " . $numcorrect . ", " . $currentcontinuous . ", " . ($lastgameperfect == true ? "true" : "false") . ") 
+				VALUES('" . $fitbit . "', " . $streak . ", " . $accuracy . ", " . $continuous . ", '" . $display . "', '" . $lastgame . "', " . $currentstreak . ", " . $numgamesplayed . ", " . $numcorrect . ", " . $currentcontinuous . ", " . $lastgameperfect . ") 
 				ON CONFLICT (fitbit) DO UPDATE 
 				SET streak = EXCLUDED.streak, 
 					accuracy = EXCLUDED.accuracy, 
