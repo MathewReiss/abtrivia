474eca681bfd33d19c391dbbe0a639962bd6e9cd
MathewReiss <MathewReiss@users.noreply.github.com>
2019-01-23 07:28:37 -0500

    Get trivia by appropriate day

--- api/index.php
+++ api/index.php
@@ -7,8 +7,15 @@
 
 	$conn = pg_connect(getenv("DATABASE_URL"));
 
-	$myquery = "SELECT * FROM questions ORDER BY day DESC LIMIT 1";
+	$day = $_GET['day'];
+
+	$myquery = "SELECT * FROM questions WHERE day='{$day}' LIMIT 1;";
 	$result = pg_query($conn, $myquery);
+
+	if(pg_num_rows($result) == 0){
+		$myquery = "SELECT * FROM questions ORDER BY day DESC LIMIT 1;";
+		$result = pg_query($conn, $myquery);
+	}
 
 	$row = pg_fetch_row($result);
 	echo '{ "date" : "' . $row[0] . '", "questions" : ' . html_entity_decode(str_replace("&quot;" , "'", str_replace("&#039;", "'", $row[1]))) . ' }';
